0aef5af95fdf1765d7c37e7ba0eee070e671c5ae
isaacyu11 <yu2000.yy@gmail.com>
2024-08-22 19:59:28 +0800

    delay off done

--- Microwave-Control.ino
+++ Microwave-Control.ino
@@ -1,6 +1,6 @@
-#define BLYNK_TEMPLATE_ID           "TMPL6y1lYUquR"
-#define BLYNK_TEMPLATE_NAME         "Quickstart Template"
-#define BLYNK_AUTH_TOKEN            "S7vZ66tb61J-KukhfM6bw8NTLxvAc_io"
+#define BLYNK_TEMPLATE_ID           "TMPL6Hbt2JWWS"
+#define BLYNK_TEMPLATE_NAME         "Microwave Control IOT"
+#define BLYNK_AUTH_TOKEN            "j7Js-LI0KwhnbSf-VC-wgmhg6Qb3-4bn"
 
 #include <WiFi.h>
 #include <WiFiClient.h>
@@ -8,41 +8,31 @@
 
 char ssid[] = "mmslab_dev";
 char pass[] = "mmslab406";
-bool led = false;
+
+bool relayOn = false;
+unsigned long relayTimer = 900000;
+unsigned long relayStartTime = 0;
 
 BlynkTimer timer;
 
 // This function is called every time the Virtual Pin 0 state changes
 BLYNK_WRITE(V0)
 {
-  // Set incoming value from pin V0 to a variable
   int value = param.asInt();
 
-  // Update state
-  Blynk.virtualWrite(V1, value);
   if(value == 1){
-    led = true;
+    relayOn = true;
   }
   else{
-    led = false;
+    relayOn = false;
   }
 }
 
-// This function is called every time the device is connected to the Blynk.Cloud
-BLYNK_CONNECTED()
+BLYNK_WRITE(V1)
 {
-  // Change Web Link Button message to "Congratulations!"
-  Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
-  Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
-  Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
-}
+  int value = param.asInt();
 
-// This function sends Arduino's uptime every second to Virtual Pin 2.
-void myTimerEvent()
-{
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V2, millis() / 1000);
+  relayTimer = value*60000;
 }
 
 void setup() {
@@ -50,12 +40,6 @@ void setup() {
   Serial.begin(115200);
 
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  // You can also specify server:
-  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
-  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
-
-  // Setup a function to be called every second
-  timer.setInterval(1000L, myTimerEvent);
 
   pinMode(2, OUTPUT); // 假设GPIO 2为输出引脚
 }
@@ -66,5 +50,19 @@ void loop() {
   // You can inject your own code or combine it with other sketches.
   // Check other examples on how to communicate with Blynk. Remember
   // to avoid delay() function!
-  digitalWrite(2, led);
+  if(relayOn)
+  {
+    relayStartTime = millis();
+    digitalWrite(2, HIGH);
+    Serial.println("relay on");
+  }
+  else if(!relayOn && millis() - relayStartTime > relayTimer)
+  {
+    digitalWrite(2,LOW);
+    Serial.println("relay off");
+  }
+  else
+  {
+    relayOn = false;
+  }
 }
